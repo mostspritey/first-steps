d393d3538462bea69536d074923f54474733c5de
mostspritey <145297398+mostspritey@users.noreply.github.com>
2023-09-17 22:01:48 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mysecondproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int nos = 3;
+
+            Console.WriteLine("your number of siblings are: " + nos);
+
+            string name = ("callum");
+
+            Console.WriteLine("your name is: " + name);
+
+            bool status = true;
+
+            Console.WriteLine("you are alive: " + status);
+
+            char symb = '@';
+
+            Console.WriteLine("your personlised symbol is: " + symb);
+
+            double pin = 4823.4545;
+
+            Console.WriteLine("your PIN is: " + pin);
+
+            Console.Beep();
+
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+;            
+        }
+    }
+}
